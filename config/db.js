c06e3b3620c47a8a6d721a22272cfa505a7fb5a1
tusharbhojwani99-beep <tusharbhojwani.99@gmail.com>
2026-09-03 11:41:41 +0530

    Fix Vercel route dest and add MONGO_URI support

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/collegeDB");
+        const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/collegeDB";
+        await mongoose.connect(mongoURI);
         console.log("MongoDB Connected");
     } catch (err) {
         console.log(err);
